130cd131ac6fd385433b6b88a73fec72d11967aa
URIEN Loic <lurien@murex.com>
2021-02-23 19:17:44 +0100

    add new monoid categories

--- chap3.cxx
+++ chap3.cxx
@@ -1,13 +1,19 @@
-#include<string>
+#include <cstdint>
+#include <string>
 
+// Helper
 template<class T>
 T not_defined(T);
 
+// Monoid concept
 template<class T>
 T mempty = not_defined(mempty<T>);
 
 template<class T>
 T mappend(const T&, const T&) = delete;
+
+template<class T>
+T mappend(T, T) = delete;
 
 template<class M>
 concept Monoid = requires(M m)
@@ -16,6 +22,7 @@ concept Monoid = requires(M m)
     mappend(m, m);
 };
 
+// String monoid
 template<>
 std::string mempty<std::string> = std::string{};
 
@@ -23,6 +30,26 @@ template<>
 std::string mappend<std::string>(const std::string& lhs, const std::string& rhs)
 {
     return lhs + rhs;
+}
+
+// Bool with op AND monoid
+template<>
+bool mempty<bool> = true;
+
+template<>
+bool mappend<bool>(bool lhs, bool rhs)
+{
+    return lhs && rhs;
+}
+
+// Addition mod 3 monoid
+template<>
+int8_t mempty<int8_t> = 0;
+
+template<>
+int8_t mappend<int8_t>(int8_t lhs, int8_t rhs)
+{
+    return (lhs + rhs) % 3;
 }
 
 int main()
